3396420c405d6b7071ee8167914a40e9f5435282
Ali Buchan <Ali Buchan Old PC Laptop>
2022-10-02 12:30:33 +0100

    Added basic imgui events needs renderer now

--- Falco/src/Falco/ImGui/ImGuiLayer.h
+++ Falco/src/Falco/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Falco/Layer.h"
+#include "Falco/Events/ApplicationEvent.h"
+#include "Falco/Events/KeyEvent.h"
+#include "Falco/Events/MouseEvent.h"
 
 namespace Falco {
 
@@ -14,6 +17,18 @@ namespace Falco {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
+	
 	private:
 		float m_Time = 0.0f;
 	};
